e4af6c264365c1c71d6c033dd43abb2e91ac9ebd
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-04-09 14:53:39 +0200

    add a return of reservation_id

--- src/reservations_ms/endpoints/reservation.py
+++ src/reservations_ms/endpoints/reservation.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Response, status
 from mongodb.mongodb_client import MongoDBClient
+from starlette.responses import JSONResponse
 
 from rabbitmq.rabbitmq_client import RabbitMQClient, PURCHASES_QUEUE_NAME, PURCHASES_EXCHANGE_NAME, \
     RESERVATIONS_QUEUE_NAME, RESERVATIONS_EXCHANGE_NAME
@@ -33,17 +34,18 @@ async def make_reservation(trip_id: str):
             "_id": insert_result.inserted_id,
             "reserved": True
         }
-        RabbitMQClient.send_data_to_queue(queue_name=PURCHASES_QUEUE_NAME, exchange_name=PURCHASES_EXCHANGE_NAME,
-                                          payload=msg_to_purchase_ms)
+        client = RabbitMQClient.get_instance()
+        client.send_data_to_queue(queue_name=PURCHASES_QUEUE_NAME, exchange_name=PURCHASES_EXCHANGE_NAME,
+                                  payload=msg_to_purchase_ms)
         msg_to_director_ms = {
             "trip_id": trip_id,
             "reserved": True,
             "payed": False
         }
-        RabbitMQClient.send_data_to_queue(queue_name=RESERVATIONS_QUEUE_NAME, exchange_name=RESERVATIONS_EXCHANGE_NAME,
-                                          payload=msg_to_director_ms)
-
-        # tmp = MongoDBClient.trips_collection.update_one(filter={"_id": xd.inserted_id}, update={"$set": {"reserved": True}})
+        client.send_data_to_queue(queue_name=RESERVATIONS_QUEUE_NAME, exchange_name=RESERVATIONS_EXCHANGE_NAME,
+                                  payload=msg_to_director_ms)
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content={"reservation_id": insert_result.inserted_id},
+                            media_type="application/json")
     except Exception:  # TODO to exception będzie zmienione na bardziej konkretne kiedy powstanie reservation service
         return Response(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=f"Something went wrong",
                         media_type="text/plain")
