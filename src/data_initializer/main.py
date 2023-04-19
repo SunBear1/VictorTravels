d4748715c6d60c493c4360716839564b00160f68
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-04-19 22:32:50 +0200

    mongo version change, mongo healthcheck change, refactor

--- src/data_initializer/main.py
+++ src/data_initializer/main.py
@@ -2,7 +2,7 @@ import json
 import logging
 import sys
 
-from db_clients import PostgreSQLClient, MongoDBClient, PG_DB_TRIPS_NAME, PG_DB_USERS_NAME
+from db_clients import PostgreSQLClient, MongoDBClient, PG_DB_TRIPS_NAME, PG_DB_USERS_NAME, MONGO_DB_NAME
 
 logger = logging.getLogger("data-init")
 
@@ -15,12 +15,13 @@ if __name__ == "__main__":
     logger.info("Data initializer started")
 
     pg_client = PostgreSQLClient()
-    pg_client.create_database(db_name=PG_DB_TRIPS_NAME)
     pg_client.create_database(db_name=PG_DB_USERS_NAME)
     pg_client.execute_query_for_database(db_name=PG_DB_USERS_NAME,
                                          query=open(file="postgresql_init_users.sql", mode="r",
                                                     encoding="utf-8").read())
     logger.info(f"Committed init {PG_DB_USERS_NAME} data to postgreSQL")
+
+    pg_client.create_database(db_name=PG_DB_TRIPS_NAME)
     pg_client.execute_query_for_database(db_name=PG_DB_TRIPS_NAME,
                                          query=open(file="postgresql_init_trips.sql", mode="r",
                                                     encoding="utf-8").read())
@@ -35,7 +36,7 @@ if __name__ == "__main__":
     except Exception as ex:
         logger.info(f"Error occured when inserting data to mongodb: {ex}")
 
-    logger.info("Committed init data to mongoDB")
+    logger.info(f"Committed init {MONGO_DB_NAME} data to mongoDB")
 
     logger.info("Finished initializing data")
     sys.exit(0)
