4c0a03ff3c23bbf0516d9a842a581b259a888915
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-06-01 16:20:40 +0200

    clean up

--- tests/gui/mock_gui_websocket_client.js
+++
@@ -1,9 +0,0 @@
-const WebSocket = require('ws');
-
-const socket = new WebSocket('ws://127.0.0.1:18000/ws/events/preferences');
-
-socket.addEventListener('message', (event) => {
-    const eventData = JSON.parse(event.data);
-    // Update the UI with the received event data
-    console.log('Received event:', eventData);
-});
