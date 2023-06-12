66421a8872d27f2a269f35e1ee90a1101d0a6f68
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-06-12 21:48:46 +0200

    Event generator fix (#62)
    * add handling of own transport in reservations and transport ms
    
    * add hotel ms, transport ms and eventhub random events logic
    
    * fixing eventhub
    
    * removing infinite loops :clownface:

--- src/event_generator/main.py
+++ src/event_generator/main.py
@@ -1,14 +1,12 @@
-import asyncio
+import ast
 import json
 import logging
 import os
 import random
+import time
 
-import uvicorn
-from fastapi import FastAPI
-
-from generate.generators import generate_hotel_price_change, generate_hotel_availability_change, \
-    generate_connection_availability_change, generate_connection_price_change
+from generate.generators import generate_hotel_price_change, generate_connection_price_change, \
+    generate_hotel_availability_change, generate_connection_availability_change
 from rabbitmq.rabbitmq_client import RabbitMQClient, RANDOM_EVENTS_PUBLISH_QUEUE_NAME, RANDOM_EVENTS_EXCHANGE_NAME
 
 logger = logging.getLogger("event-generator")
@@ -17,44 +15,29 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
-GENERATION_FREQUENCY = int(os.getenv("GENERATION_FREQUENCY", 10))
 
-app = FastAPI()
-message_id = 1
-event_generating_functions = [generate_hotel_price_change, generate_connection_price_change,
-                              generate_hotel_availability_change, generate_connection_availability_change]
-
-
-@app.get("/api/v1/healthcheck")
-def healthcheck():
-    return {"status": "OK"}
-
-
-def generate_random_event():
-    global message_id
-    event_generation_function = random.choice(event_generating_functions)
-    random_payload = event_generation_function()
-    random_payload["id"] = message_id
-    event_generator_client = RabbitMQClient()
-    event_generator_client.send_data_to_queue(queue_name=RANDOM_EVENTS_PUBLISH_QUEUE_NAME,
-                                              payload=json.dumps(random_payload, ensure_ascii=False).encode(
-                                                  'utf-8'),
-                                              exchange_name=RANDOM_EVENTS_EXCHANGE_NAME)
-    event_generator_client.close_connection()
-    message_id += 1
-
-
-async def run_background_task():
-    while True:
-        generate_random_event()
-        await asyncio.sleep(GENERATION_FREQUENCY)
-
-
-@app.on_event("startup")
-async def startup_event():
-    loop = asyncio.get_event_loop()
-    loop.create_task(run_background_task())
-
+ACTIVATE_GENERATION = ast.literal_eval(os.getenv("ACTIVATE_GENERATION", "True"))
+GENERATION_FREQUENCY = int(os.getenv("GENERATION_FREQUENCY", 5))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    logger.info("Random events generator started")
+    event_generator_client = RabbitMQClient()
+
+    message_id = 1
+    event_generating_functions = [generate_hotel_price_change, generate_connection_price_change,
+                                  generate_hotel_availability_change, generate_connection_availability_change]
+
+    logger.info(f"Generating events mode is set to: {ACTIVATE_GENERATION}")
+    while True:
+        time.sleep(GENERATION_FREQUENCY)
+        if not ACTIVATE_GENERATION:
+            continue
+        event_generation_function = random.choice(event_generating_functions)
+        random_payload = event_generation_function()
+        random_payload["id"] = message_id
+
+        event_generator_client.send_data_to_queue(queue_name=RANDOM_EVENTS_PUBLISH_QUEUE_NAME,
+                                                  payload=json.dumps(random_payload, ensure_ascii=False).encode(
+                                                      'utf-8'),
+                                                  exchange_name=RANDOM_EVENTS_EXCHANGE_NAME)
+        message_id += 1
