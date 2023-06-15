5a1543098246689b218be3ddc82d4e055cb86d54
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2023-06-16 00:24:41 +0200

    Researcher consumer fix (#72)
    * added new trip to egypt
    
    * fix data-initializer
    
    * fixed consuming messages in researcher ms
    
    * event payload alignments

--- src/trips_ms/src/main/java/com/cringe/travels/trips/rabbitmq/MsgTypes.java
+++ src/trips_ms/src/main/java/com/cringe/travels/trips/rabbitmq/MsgTypes.java
@@ -1,14 +1,17 @@
 package com.cringe.travels.trips.rabbitmq;
 
 public enum MsgTypes {
-    HOTEL_ROOMS_UPDATE("hotel_rooms_update"),
+    HOTEL_ROOMS_UPDATE("hotel_rooms_availability_update"),
+    HOTEL_PRICE_UPDATE("hotel_room_price_update"),
     HOTEL_BOOKINGS_STATUS("hotel_booking_status"),
     TRANSPORT_UPDATE("transport_update"),
-    TRANSPORT_BOOKING_STATUS("transport_booking_status");
+    TRANSPORT_BOOKING_STATUS("transport_booking_status"),
+    TRANSPORT_GENERATED_SEATS_UPDATE("generated_transport_availability_update"),
+    TRANSPORT_GENERATED_PRICE_UPDATE("generated_transport_price_update");
 
     public final String label;
 
-    private MsgTypes(String label) {
+    MsgTypes(String label) {
         this.label = label;
     }
 }
